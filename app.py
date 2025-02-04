59d8f94dcc51692ff0490655617de5e6747d8e01
Jair Torezone <jairtorezone@gmail.com>
2025-02-04 20:16:16 -0300

    Criando Página de Pagamento

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, send_file
+from flask import Flask, jsonify, request, send_file, render_template
 from repository.database import db
 from db_models.payment import Payment
 from datetime import datetime, timedelta
@@ -30,7 +30,6 @@ def create_payment_pix():
   db.session.add(new_payment)
   db.session.commit()
 
-
   return jsonify({"message": "The payment has been created",
                   "payment": new_payment.to_dict()})
 
@@ -44,7 +43,7 @@ def pix_confirmation():
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
-  return 'pagamento pix'
+  return render_template('payment.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
